06c1b4dfcafca1c1378c128483039ea2579a28b3
Emesson-cmd <emerssosnkavallcante@gmail.com>
2025-09-12 09:26:49 -0300

    feat: Create login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,26 @@
-import SurprisePage from './components/SurprisePage';
-
 export default function Home() {
-  return <SurprisePage />;
+  return (
+    <div className="container mx-auto p-4">
+      <form className="flex flex-col space-y-4 max-w-sm mx-auto">
+        <h1 className="text-2xl font-bold mb-4 uppercase">Sign Up</h1>
+        <input type="text" placeholder="Email" className="border p-2 rounded" />
+        <input
+          type="password"
+          placeholder="Password"
+          className="border p-2 rounded"
+        />
+        <input
+          type="password"
+          placeholder="Repeat password"
+          className="border p-2 rounded"
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 text-white py-2 px-4 rounded uppercase font-semibold hover:bg-blue-600 transition-colors "
+        >
+          Create account
+        </button>
+      </form>
+    </div>
+  );
 }
